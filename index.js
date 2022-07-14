2bf782833751802f8fde4053b8a277ad878c766a
ykuruvilla <yasha.kuruvilla@gmail.com>
2022-07-14 13:00:41 +0100

    add and enable cors package

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+app.use(cors());
+
 const videoRouter = require("./routes/videos");
 
 const PORT = process.env.PORT || 8080;
