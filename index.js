df89e0467b92d29a1e5b281d00d212ce0d7522d4
ykuruvilla <yasha.kuruvilla@gmail.com>
2022-09-30 10:13:09 +0100

    minor code cleanup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const path = require("path");
 app.use(express.json());
 
 const cors = require("cors");
@@ -16,6 +17,8 @@ app.use("/videos", videoRouter);
 app.get("/", (_request, response) => {
   response.status(200).send("<h1>Hello there!</h1>");
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
